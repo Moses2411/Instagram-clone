38bb2c6c2ea4377a74184d27bbfe6096cd167083
Moses2411 <122967257+Moses2411@users.noreply.github.com>
2025-11-12 16:15:10 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL ='sqlite:///./instagram.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./instagram.db")
 
-SessionLocal = sessionmaker(bind=engine, autocommit = False, autoflush=False)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
